33f8877997ae6ab6e0e63bb1b8af0f3833f5e1c5
Randy <35547932+WolvenRA@users.noreply.github.com>
2021-10-08 18:27:43 -0700

    Add files via upload

---
+++ ZipExtractor/FormMain.Designer.vb
@@ -0,0 +1,95 @@
+﻿Namespace ZipExtractor
+
+  <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+  Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+      Try
+        If disposing AndAlso components IsNot Nothing Then
+          components.Dispose()
+        End If
+      Finally
+        MyBase.Dispose(disposing)
+      End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+      Me.labelInformation = New System.Windows.Forms.Label()
+      Me.pictureBoxIcon = New System.Windows.Forms.PictureBox()
+      Me.progressBar = New System.Windows.Forms.ProgressBar()
+      CType(Me.pictureBoxIcon, System.ComponentModel.ISupportInitialize).BeginInit()
+      Me.SuspendLayout()
+      '
+      'labelInformation
+      '
+      Me.labelInformation.AutoSize = True
+      Me.labelInformation.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+      Me.labelInformation.Location = New System.Drawing.Point(94, 16)
+      Me.labelInformation.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+      Me.labelInformation.Name = "labelInformation"
+      Me.labelInformation.Size = New System.Drawing.Size(85, 17)
+      Me.labelInformation.TabIndex = 6
+      Me.labelInformation.Text = "Extracting..."
+      '
+      'pictureBoxIcon
+      '
+      Me.pictureBoxIcon.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+              Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+      Me.pictureBoxIcon.Image = Global.ZipExtractor.My.Resources.Resources.ZipExtractor
+      Me.pictureBoxIcon.Location = New System.Drawing.Point(13, 13)
+      Me.pictureBoxIcon.Margin = New System.Windows.Forms.Padding(5)
+      Me.pictureBoxIcon.Name = "pictureBoxIcon"
+      Me.pictureBoxIcon.Size = New System.Drawing.Size(72, 72)
+      Me.pictureBoxIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+      Me.pictureBoxIcon.TabIndex = 5
+      Me.pictureBoxIcon.TabStop = False
+      '
+      'progressBar
+      '
+      Me.progressBar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+              Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+      Me.progressBar.Location = New System.Drawing.Point(96, 47)
+      Me.progressBar.Margin = New System.Windows.Forms.Padding(5)
+      Me.progressBar.Name = "progressBar"
+      Me.progressBar.Size = New System.Drawing.Size(396, 35)
+      Me.progressBar.TabIndex = 4
+      '
+      'FormMain
+      '
+      Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+      Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+      Me.AutoSize = True
+      Me.ClientSize = New System.Drawing.Size(504, 99)
+      Me.Controls.Add(Me.labelInformation)
+      Me.Controls.Add(Me.pictureBoxIcon)
+      Me.Controls.Add(Me.progressBar)
+      Me.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+      Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+      Me.Margin = New System.Windows.Forms.Padding(4)
+      Me.MaximizeBox = False
+      Me.MinimizeBox = False
+      Me.Name = "FormMain"
+      Me.ShowIcon = False
+      Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+      Me.Text = "Installing update..."
+      CType(Me.pictureBoxIcon, System.ComponentModel.ISupportInitialize).EndInit()
+      Me.ResumeLayout(False)
+      Me.PerformLayout()
+
+    End Sub
+    Private WithEvents labelInformation As System.Windows.Forms.Label
+    Private WithEvents pictureBoxIcon As System.Windows.Forms.PictureBox
+    Private WithEvents progressBar As System.Windows.Forms.ProgressBar
+  End Class
+
+End Namespace
---
+++ ZipExtractor/FormMain.vb
@@ -0,0 +1,191 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Text
+Imports System.Windows.Forms
+Imports ZipExtractor.My.Resources
+
+Namespace ZipExtractor
+
+  Public Class FormMain
+
+    Private Started As Boolean = False
+    Private _backgroundWorker As BackgroundWorker
+    Private TotProgressPercentage As UInteger
+
+    ReadOnly _logBuilder As StringBuilder = New StringBuilder()
+
+    Private Sub FormMain_Load(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles Me.Load
+
+      Try
+        If Started = False Then
+          Begin()
+        End If
+
+      Catch exception As Win32Exception
+        If exception.NativeErrorCode <> 1223 Then Throw
+      End Try
+    End Sub
+
+    Private Sub Begin()
+
+      Started = True
+
+      TotProgressPercentage = 0
+
+      _logBuilder.AppendLine(DateTime.Now.ToString("F"))
+      _logBuilder.AppendLine()
+      _logBuilder.AppendLine("ZipExtractor started with following command line arguments.")
+
+      Dim args As String() = Environment.GetCommandLineArgs()
+      labelInformation.Text = "Args = " & args.Length.ToString()
+
+'      For Each arg In args
+'        MessageBox.Show(arg)
+'      Next
+
+      For index = 0 To args.Length - 1
+        Dim arg = args(index)
+        _logBuilder.AppendLine("[" & index & "] " & arg)
+      Next
+
+      _logBuilder.AppendLine()
+
+      If args.Length >= 4 Then
+        Dim executablePath As String = args(3)
+
+        _backgroundWorker = New BackgroundWorker With {
+          .WorkerReportsProgress = True,
+          .WorkerSupportsCancellation = True}
+
+        AddHandler _backgroundWorker.DoWork, _
+          Sub(o, eventArgs)
+
+            For Each Wrk_Process In Process.GetProcesses()
+              Try
+                If Wrk_Process.MainModule.FileName.Equals(args(2)) Then
+                  _logBuilder.AppendLine("Waiting for application Wrk_Process to Exit...")
+                  labelInformation.Text = "Waiting for application to Exit..."
+                  _backgroundWorker.ReportProgress(0, "Waiting for application to Exit...")
+                  Wrk_Process.WaitForExit()
+                End If
+
+              Catch exception As Exception
+                Debug.WriteLine(exception.Message)
+              End Try
+            Next
+
+            _logBuilder.AppendLine("BackgroundWorker started successfully.")
+'            labelInformation.Text = "BackgroundWorker started successfully."
+
+            Dim path = args(2)
+            Dim DirPath = System.IO.Path.GetDirectoryName(args(2))
+            Dim zip As ZipStorer = ZipStorer.Open(args(1), FileAccess.Read)
+            Dim dir As List(Of ZipStorer.ZipFileEntry) = zip.ReadCentralDir()
+
+            _logBuilder.AppendLine("Found total of " & dir.Count & "files and folders inside the zip file.")
+            '            labelInformation.Text = "Found total of " & dir.Count & "files and folders inside the zip file."
+
+            For index = 0 To dir.Count - 1
+              If _backgroundWorker.CancellationPending Then
+                eventArgs.Cancel = True
+                zip.Close()
+                Return
+              End If
+
+              Dim entry As ZipStorer.ZipFileEntry = dir(index)
+'              zip.ExtractFile(entry, System.IO.Path.Combine(DirPath, entry.FilenameInZip))
+              zip.ExtractFile(entry, System.IO.Path.Combine(path, entry.FilenameInZip))
+
+              Dim currentFile As String = String.Format(My.Resources.CurrentFileExtracting, entry.FilenameInZip)
+              Dim progress As Integer = (index + 1) * 100 / dir.Count
+
+              _backgroundWorker.ReportProgress(progress, currentFile)
+              _logBuilder.AppendLine(currentFile & " [" & progress & "%]")
+'              labelInformation.Text = currentFile & " [" & progress & "%]"
+            Next
+
+            zip.Close()
+          End Sub
+
+        AddHandler _backgroundWorker.ProgressChanged, _
+        Sub(o, eventArgs)
+          progressBar.Value = eventArgs.ProgressPercentage
+          labelInformation.Text = eventArgs.UserState.ToString()
+        End Sub
+
+        AddHandler _backgroundWorker.RunWorkerCompleted, _
+        Sub(o, eventArgs)
+
+          Try
+            If eventArgs.[Error] IsNot Nothing Then
+              Throw eventArgs.[Error]
+            End If
+
+            If Not eventArgs.Cancelled Then
+              labelInformation.Text = "Finished"
+
+              Try
+                '                Dim processStartInfo As ProcessStartInfo = New ProcessStartInfo(args(3))
+                Dim processStartInfo As ProcessStartInfo = New ProcessStartInfo(executablePath)
+
+                If args.Length > 4 Then
+                  processStartInfo.Arguments = args(4)
+                End If
+
+                Process.Start(processStartInfo)
+
+                _logBuilder.AppendLine("Successfully launched the updated application.")
+                labelInformation.Text = "Successfully launched the updated application."
+
+              Catch exception As Win32Exception
+
+                If exception.NativeErrorCode <> 1223 Then
+                  Throw
+                End If
+              End Try
+            End If
+
+          Catch exception As Exception
+            _logBuilder.AppendLine()
+            _logBuilder.AppendLine(exception.ToString())
+
+            MessageBox.Show(exception.Message, exception.[GetType]().ToString(), MessageBoxButtons.OK, _
+                            MessageBoxIcon.[Error])
+
+          Finally
+            _logBuilder.AppendLine()
+            Application.Exit()
+          End Try
+        End Sub
+
+        _backgroundWorker.RunWorkerAsync()
+      End If
+
+    End Sub
+
+    Private Sub FormMain_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) _
+    Handles Me.FormClosing
+
+      '      Do While TotProgressPercentage < 100
+      '      TotProgressPercentage += 1
+      '      progressBar.Value = TotProgressPercentage
+      '      Loop
+
+      If _backgroundWorker IsNot Nothing Then
+        _backgroundWorker.CancelAsync()
+      End If
+
+      _logBuilder.AppendLine()
+
+      File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ZipExtractor.log"), _logBuilder.ToString())
+
+    End Sub
+
+  End Class
+
+End Namespace
